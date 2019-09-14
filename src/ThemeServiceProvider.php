d473f309b822f9cda9c2acec13698e3cd3d2f498
Wouter <wouter@subspace.ga>
2019-09-14 18:45:57 +0200

    adjustments

--- src/ThemeServiceProvider.php
+++ src/ThemeServiceProvider.php
@@ -1,7 +1,25 @@
 <?php
 namespace WingingIT\Themes;
 
-class ThemeServiceProvider
+class ThemeServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+        ]);
+    }
 
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
 }
